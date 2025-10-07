3ae3297add9c054c1934970445b606df649c9a93
Ole <o.komorowski@gmx.de>
2025-10-07 18:53:52 +0200

    navigator left upper header

--- apps/web/src/app/(protected)/(header)/LoggedInLeftHeaderComponent.tsx
+++ apps/web/src/app/(protected)/(header)/LoggedInLeftHeaderComponent.tsx
@@ -1,5 +1,33 @@
+'use client'
+import { Button } from '@/components/ui/button'
 import { User } from '@supabase/supabase-js'
+import { useRouter, usePathname } from 'next/navigation'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function LoggedInLeftHeaderComponent({ user }: { user: User | null }) {
-  return <div>{user?.aud}</div>
+  void user
+  const router = useRouter()
+  const pathname = usePathname()
+  const [destination, setDestination] = useState('/recipes')
+
+  useEffect(() => {
+    if (pathname === '/recipes') {
+      setDestination('Home')
+    } else {
+      setDestination('Rezepteübersicht')
+    }
+  }, [pathname])
+
+  const clickHandler = useCallback(() => {
+    if (pathname === '/recipes') {
+      router.push('/app')
+    } else {
+      router.push('/recipes')
+    }
+  }, [pathname])
+  return (
+    <div>
+      <Button onClick={clickHandler}>{destination}</Button>
+    </div>
+  )
 }
--- apps/web/src/app/(protected)/(header)/layout.tsx
+++ apps/web/src/app/(protected)/(header)/layout.tsx
@@ -2,38 +2,27 @@ import type { ReactNode } from 'react'
 import { supabaseServerRSC } from '@/lib/supabase/server-rsc'
 import { Header } from '@/components/layout/Header'
 import { Button } from '@/components/ui/button'
-import Link from 'next/link'
 import AccountDropDownMenu from '@/components/AccountDropDownMenu'
 import LoggedInLeftHeaderComponent from './LoggedInLeftHeaderComponent'
+import { redirect } from 'next/navigation'
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const s = await supabaseServerRSC()
   const {
     data: { user },
   } = await s.auth.getUser()
+  if (!user) {
+    redirect('/log-in/password')
+  }
   return (
     <>
       <Header
         left={
-          user ? (
-            <Button>
-              <LoggedInLeftHeaderComponent user={user} />
-            </Button>
-          ) : (
-            <Button>
-              <Link href="/log-in-or-create-account">logo</Link>
-            </Button>
-          )
+          <Button>
+            <LoggedInLeftHeaderComponent user={user} />
+          </Button>
         }
-        right={
-          user ? (
-            <AccountDropDownMenu user={user.email}></AccountDropDownMenu>
-          ) : (
-            <Button>
-              <Link href="/log-in-or-create-account">Anmelden</Link>
-            </Button>
-          )
-        }
+        right={<AccountDropDownMenu user={user?.email}></AccountDropDownMenu>}
       />
       {children}
     </>
